d49d7613c283e78f3e85e58bb6cbac9a62ee2b25
Jacob <psves0512@gmail.com>
2022-12-22 15:35:52 +0800

    current

--- src/components/Common.vue
+++ src/components/Common.vue
@@ -312,9 +312,10 @@
     </div>
     
     <!-- chat controler -->
-    <div class="chat_controler" @click="is_chat = !is_chat">
+    <div class="chat_controler" v-show="customerService.Text || customerService.CSText || customerService.FBText" 
+      @click="is_chat = !is_chat">
       <i class="fa-solid fa-comment-dots" v-if="!is_chat"></i>
-      <i class="fa-solid fa-xmark" v-else></i>
+      <i class="fa-solid fa-comment-slash" v-else></i>
     </div>
 
     <!-- fb -->
@@ -354,6 +355,8 @@ export default {
 
       //
       is_chat: false,
+      isTawkAddClick: false,
+      isTawkOpen: false,
 
       // search
       is_search: 0,
@@ -401,52 +404,121 @@ export default {
       this.favorite = JSON.parse(this.json_favorite);
     },
     is_chat(val) {
+      let vm = this;
       let arr = [];
 
-      let tawk = document.querySelector('.widget-visible iframe')
-      tawk.style.transition = '.5s';
-      let tawkStyle;
-      if (tawk) {
-        tawkStyle = window.getComputedStyle(tawk);
-        arr.push(tawk)
-      }
-      
-      let line = document.querySelector('.line_icon')
-      line.style.transition = '.5s';
-      let lineStyle;
-      if (line) {
-        lineStyle = window.getComputedStyle(line);
-        arr.push(line)
+      let tawkbutton;
+      let tawkchat;
+      let tawks = document.querySelectorAll('.widget-visible iframe')
+      if (tawks.length) {
+        tawks[0].style.transition = '.5s';
+        tawkbutton = tawks[0].contentWindow.document.querySelector('button');
+        tawkchat = tawks[1];
+
+        // 綁定 click 
+        if(!this.isTawkAddClick) {
+          tawkbutton.addEventListener('click', function() {
+            if(vm.isTawkOpen) {
+              setTimeout(()=>{
+                tawkchat.style.bottom = parseInt(window.getComputedStyle(tawks[0]).bottom) + 70 + 'px';
+                tawkchat.style.right = 95 + 'px';
+              }, 0)
+            } else {
+              tawkchat.style.bottom = parseInt(window.getComputedStyle(tawks[0]).bottom) + 'px';
+              tawkchat.style.right = 95 + 'px';
+            }
+          })
+          this.isTawkAddClick = true
+        }
+
+        if(this.isTawkOpen) {
+          tawkbutton.click()
+          this.isTawkOpen = false;
+        }
+
+        arr.push(tawks)
       }
 
+      let line = document.querySelector('.line_icon')
+      if (line) arr.push(line)
+
+      let fbchat;
       let fbs = document.querySelectorAll('.fb_dialog iframe')
-      fbs[0].style.transition = '.5s';
-      fbs[1].style.transition = '.5s';
-      fbs[2].style.transition = '.5s';
-      if(fbs) {
+      if(fbs.length) {
+        for(let item of fbs) {
+          item.style.transition = '.5s';
+        }
+        fbchat = document.querySelector('.fb_iframe_widget iframe')
+        fbchat.style.height =  550 + 'px';
+        fbchat.style.right =  95 + 'px';
         arr.push(fbs)
       }
 
       if(val) {
         let bottom = 90;
         for(let item of arr) {
-          if(item.length) {
-            item[0].style.bottom = bottom + 'px'
-            item[1].style.bottom = bottom + 'px'
-            item[2].style.bottom = bottom + 'px'
-          } else {
+          // line
+          if(!item.length) {
             item.style.bottom = bottom + 'px'
           }
+          // tawk 
+          else if(item[0].parentNode.classList.contains('widget-visible')) {
+            // chat 開關
+            if(tawkchat.classList.contains('open')){
+              setTimeout(()=>{
+                tawkchat.style.display = 'block'; 
+              }, 200)
+            }
+
+            // icon 移動
+            item[0].style.bottom = bottom + 'px'
+          }
+          // fb
+          else {
+            // chat 開關
+            if(fbchat.style.maxHeight != '0px'){
+              fbchat.style.display = 'block'
+            }
+
+            // icon 移動
+            item[0].style.bottom = bottom + 'px'
+            item[1].style.bottom = bottom - 2.5 + 'px';
+            item[2].style.bottom = bottom + 44 + 'px';
+
+            // chat 移動
+            fbchat.style.bottom =  bottom - 10 + 'px';
+          }
+
           bottom += 70;
         }
-      } else {
+      }
+      else {
         for(let item of arr) {
-          if(item.length) {
-            item[0].style.bottom = '20px'
-            item[1].style.bottom = '20px'
-            item[2].style.bottom = '20px'
-          } else {
+          // line
+          if(!item.length) {
             item.style.bottom = '20px'
+          }
+          // tawk
+          else if(item[0].parentNode.classList.contains('widget-visible')) {
+            // chat 開關
+            if(tawkchat.classList.contains('open')){
+              tawkchat.style.display = 'none';
+            }
+
+            // icon 移動
+            item[0].style.bottom = '20px'
+          }
+          // fb
+          else {
+            // chat 開關
+            if(fbchat.style.maxHeight != '0px'){
+              fbchat.style.display = 'none'
+            }
+
+            // icon 移動
+            for(let item2 of item) {
+              item2.style.bottom = '20px'
+            }
           }
         }
       }
@@ -649,7 +721,6 @@ export default {
     prev(){
       window.history.go(-1);
     }
-
   },
   created(){
     this.api = location.host;
@@ -661,8 +732,53 @@ export default {
     }
   },
   mounted(){
+    let vm = this;
     window.addEventListener('scroll', this.scrollHandler);
     window.addEventListener('resize', this.resizeHandler);
+    window.addEventListener("webkitAnimationStart", function(event){
+      if (event.animationName === "fbchatInserted") {
+        let iframe = event.target.querySelector('iframe');
+
+        let observer = new MutationObserver(function(mutations) {
+          mutations.forEach(function(mutation) {
+            if(mutation.attributeName == 'style') {
+              if(mutation.target.style.maxHeight.indexOf('px') > -1) {
+                let fb = document.querySelector('#fb-root')
+                let icon = document.querySelector('.fb_dialog iframe')
+                // icon 移動
+                icon.style.bottom = '20px';
+                icon.style['box-shadow'] = 'none'; 
+                // icon 顯示
+                setTimeout(()=>{
+                  fb.style.opacity = 1;
+                }, 1000)
+
+                // chat 開關
+                console.log('chatHeight', mutation.target.style.maxHeight)
+                if(mutation.target.style.maxHeight != '0px'){
+                  // 隱藏
+                  iframe.style.display = 'none';
+                }
+                // 取消
+                observer.disconnect();
+              }
+            }
+          });
+        });
+        observer.observe(iframe, {attributes: true});
+      }
+      else if(event.animationName === "tawkchatInserted") {
+        let iframe = event.target;
+        let iframeStyle = window.getComputedStyle(iframe)
+        if(iframeStyle.display.indexOf('block') > -1){
+          setTimeout(()=>{
+            let tawkbutton = document.querySelector('.widget-visible iframe').contentWindow.document.querySelector('button');
+            tawkbutton.click();
+            vm.isTawkOpen = true;
+          }, 0)
+        }
+      }
+    });
   }
 }
 </script>
