62598c6e55998108f47cbfa4347f7f3addde7386
DESKTOP-T545D7R\admin <>
2023-04-21 17:42:22 +0800

    cookie 元件化，footer a 標籤樣式，isUniqm控制打包頁面

--- src/components/Common.vue
+++ src/components/Common.vue
@@ -310,10 +310,7 @@
         </ul>
       </div>
 
-      <div class="cookie">
-        <div class="close"> <i class="fa-solid fa-xmark"></i> </div>
-        本網站中使用 cookie，欲查詢有關本網站使用 cookie 方式之詳情，及若您不希望在電腦上使用 cookie 時應如何變更電腦的 cookie 設定，請參閱本網站「隱私權及網站使用條款」之 Cookie 聲明。您繼續使用本網站即表示您同意本公司得按本網站使用條款之 Cookie 聲明使用 cookie。
-      </div>
+      <Cookie />
     </div>
     
     <!-- chat controler -->
@@ -335,8 +332,12 @@
 </template>
 
 <script>
+import Cookie from './Cookie.vue'
 
 export default {
+  component: {
+    Cookie
+  },
   props: ['json_site', 'json_all', 'json_store', 'json_footer_community', 'json_copyRight', 'json_customerService', 'json_carts', 'json_favorite', 'user_account'],
   data(){
     return{
@@ -758,11 +759,6 @@ export default {
     prev(){
       window.history.go(-1);
     },
-
-    removeCookie() {
-      let node = document.querySelector('.cookie')
-      node.parentNode.removeChild(node)
-    }
   },
   created(){
     this.api = location.host;
@@ -821,13 +817,6 @@ export default {
         }
       }
     });
-
-    let isCookie = localStorage.getItem('isCookie')
-    if(isCookie) vm.removeCookie() 
-    else {
-      localStorage.setItem('isCookie', true)
-      document.querySelector('.cookie .close').addEventListener('click', vm.removeCookie)
-    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,90 +1,93 @@
+let isUniqm = true
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  pages:{
-    index : {
-      entry: 'src/pages/index/main.js', // page 的入口
-      template: 'src/pages/index/index.html', // 模板来源
-      filename: 'index.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
+  publicPath: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'uniqm' ? './' : '/',
+  pages: !isUniqm
+    ? {
+      index : {
+        entry: 'src/pages/index/main.js', // page 的入口
+        template: 'src/pages/index/index.html', // 模板来源
+        filename: 'index.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'index']
+      },
+      allProducts : {
+        entry: 'src/pages/allProducts/main.js',
+        template: 'src/pages/allProducts/index.html',
+        filename: 'allProducts.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'allProducts']
+      },
+      category : {
+        entry: 'src/pages/category/main.js',
+        template: 'src/pages/category/index.html',
+        filename: 'category.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'category']
+      },
+      rich : {
+        entry: 'src/pages/rich/main.js',
+        template: 'src/pages/rich/index.html',
+        filename: 'rich.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'rich']
+      },
+      contact : {
+        entry: 'src/pages/contact/main.js',
+        template: 'src/pages/contact/index.html',
+        filename: 'contact.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'contact']
+      },
+      search : {
+        entry: 'src/pages/search/main.js',
+        template: 'src/pages/search/index.html',
+        filename: 'search.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'search']
+      },
+      order : {
+        entry: 'src/pages/order/main.js',
+        template: 'src/pages/order/index.html',
+        filename: 'order.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'order']
+      },
+      user : {
+        entry: 'src/pages/user/main.js',
+        template: 'src/pages/user/index.html',
+        filename: 'user.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'user']
+      },
+      user_info : {
+        entry: 'src/pages/user_info/main.js',
+        template: 'src/pages/user_info/index.html',
+        filename: 'user_info.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'user_info']
+      },
+      error : {
+        entry: 'src/pages/error/main.js',
+        template: 'src/pages/error/index.html',
+        filename: 'error.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'error']
+      },
+    }
+    : {
+      shopping : {
+        entry: 'src/pages/shopping/main.js',
+        template: 'src/pages/shopping/index.html',
+        filename: 'shopping.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'shopping']
+      },
+      shoppingUser : {
+        entry: 'src/pages/shoppingUser/main.js',
+        template: 'src/pages/shoppingUser/index.html',
+        filename: 'shoppingUser.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'shoppingUser']
+      },
+      shoppingInfo : {
+        entry: 'src/pages/shoppingInfo/main.js',
+        template: 'src/pages/shoppingInfo/index.html',
+        filename: 'shoppingInfo.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'shoppingInfo']
+      },
+      shoppingOrder : {
+        entry: 'src/pages/shoppingOrder/main.js',
+        template: 'src/pages/shoppingOrder/index.html',
+        filename: 'shoppingOrder.html', 
+        chunks: ['chunk-vendors', 'chunk-common', 'shoppingOrder']
+      },
     },
-    allProducts : {
-      entry: 'src/pages/allProducts/main.js',
-      template: 'src/pages/allProducts/index.html',
-      filename: 'allProducts.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'allProducts']
-    },
-    category : {
-      entry: 'src/pages/category/main.js',
-      template: 'src/pages/category/index.html',
-      filename: 'category.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'category']
-    },
-    rich : {
-      entry: 'src/pages/rich/main.js',
-      template: 'src/pages/rich/index.html',
-      filename: 'rich.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'rich']
-    },
-    contact : {
-      entry: 'src/pages/contact/main.js',
-      template: 'src/pages/contact/index.html',
-      filename: 'contact.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'contact']
-    },
-    search : {
-      entry: 'src/pages/search/main.js',
-      template: 'src/pages/search/index.html',
-      filename: 'search.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'search']
-    },
-    order : {
-      entry: 'src/pages/order/main.js',
-      template: 'src/pages/order/index.html',
-      filename: 'order.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'order']
-    },
-    user : {
-      entry: 'src/pages/user/main.js',
-      template: 'src/pages/user/index.html',
-      filename: 'user.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'user']
-    },
-    user_info : {
-      entry: 'src/pages/user_info/main.js',
-      template: 'src/pages/user_info/index.html',
-      filename: 'user_info.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'user_info']
-    },
-    error : {
-      entry: 'src/pages/error/main.js',
-      template: 'src/pages/error/index.html',
-      filename: 'error.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'error']
-    },
-
-    shopping : {
-      entry: 'src/pages/shopping/main.js',
-      template: 'src/pages/shopping/index.html',
-      filename: 'shopping.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'shopping']
-    },
-    shoppingUser : {
-      entry: 'src/pages/shoppingUser/main.js',
-      template: 'src/pages/shoppingUser/index.html',
-      filename: 'shoppingUser.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'shoppingUser']
-    },
-    shoppingInfo : {
-      entry: 'src/pages/shoppingInfo/main.js',
-      template: 'src/pages/shoppingInfo/index.html',
-      filename: 'shoppingInfo.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'shoppingInfo']
-    },
-    shoppingOrder : {
-      entry: 'src/pages/shoppingOrder/main.js',
-      template: 'src/pages/shoppingOrder/index.html',
-      filename: 'shoppingOrder.html', 
-      chunks: ['chunk-vendors', 'chunk-common', 'shoppingOrder']
-    },
-  }, 
 }
